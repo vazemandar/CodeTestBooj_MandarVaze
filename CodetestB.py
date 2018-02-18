9beb55bb7c9eda6045c02dc4e47521749902c2a8
Mandar Vaze <vazemandar00@gmail.com>
2018-02-17 23:28:57 -0600

    Functions added

--- CodetestB.py
+++ CodetestB.py
@@ -2,60 +2,48 @@ import urllib2
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-tree = ET.ElementTree(file=urllib2.urlopen('http://syndication.enterprise.websiteidx.com/feeds/BoojCodeTest.xml'))
-root = tree.getroot()
-Listings=pd.DataFrame(columns=['MlsId', 'MlsName', 'DateListed', 'StreetAddress', 'Price', 'Bedrooms', 'Bathrooms', 'Appliances', 'Rooms', 'Description'])
+def loadData(Link): 
+    tree = ET.ElementTree(file=urllib2.urlopen(Link))
+    root = tree.getroot()
+    Listings=pd.DataFrame(columns=['MlsId', 'MlsName', 'DateListed', 'StreetAddress', 'Price', 'Bedrooms', 'Bathrooms', 'Appliances', 'Rooms', 'Description'])    
+    for i, Listing in enumerate(root.findall('Listing')):        
+        if Listing.find('ListingDetails').find('DateListed').text[:4]=='2016' and ' and ' in Listing.find('BasicDetails').find('Description').text:       
+                misid_= Listing.find('ListingDetails/MlsId').text 
+                MlsName_=Listing.find('ListingDetails/MlsName').text 
+                DateListed_=Listing.find('ListingDetails/DateListed').text
+                StreetAddress=Listing.find('Location/StreetAddress').text       
+                Price=Listing.find('ListingDetails/Price').text  
+                Bedrooms= Listing.find('BasicDetails/Bedrooms').text  
+                Bathrooms=Listing.find('BasicDetails/Bathrooms').text  
+                elemList=[]
+                for appl in Listing.find('RichDetails').find('Appliances').findall('Appliance'):
+                    elemList.append(appl.text)
+                Appliances=','.join(elemList)
+                rmlist=[]
+                try:
+                    for rm  in Listing.find('RichDetails').find('Rooms').findall('Room'): 
+                        rmlist.append(rm.text)  
+                    Room=','.join(rmlist)
+                except AttributeError :
+                    Room=''
+                Description = Listing.find('BasicDetails').find('Description').text[:200] 
+                Listings.loc[i] = [misid_,MlsName_,DateListed_,StreetAddress,Price,Bedrooms,Bathrooms,Appliances,Room,Description]
+    return Listings    
 
-for i, Listing in enumerate(root.findall('Listing')):        
-    if Listing.find('ListingDetails').find('DateListed').text[:4]=='2016' and ' and ' in Listing.find('BasicDetails').find('Description').text:       
-        try:
-            misid_= Listing.find('ListingDetails').find('MlsId').text 
-        except :
-            misid_=''
-        try:        
-            MlsName_=Listing.find('ListingDetails').find('MlsName').text
-        except :
-            MlsName=''        
-        try:   
-            DateListed=Listing.find('ListingDetails').find('DateListed').text
-        except :
-            DateListed=''
-        try:    
-            StreetAddress=Listing.find('Location').find('StreetAddress').text       
-        except :
-            StreetAddress=''           
-        try:
-            Price=Listing.find('ListingDetails').find('Price').text
-        except :
-            Price=''            
-        try:
-            Bedrooms= Listing.find('BasicDetails').find('Bedrooms').text
-        except :    
-            Bedrooms=''
-        try: 
-            Bathrooms=Listing.find('BasicDetails').find('Bathrooms').text
-        except :
-            Bathrooms=''    
-        try:    
-            Appliances=''
-            for appl in Listing.find('RichDetails').find('Appliances').findall('Appliance'):
-                Appliances= Appliances+','+ appl.text
-            Appliances=Appliances[1:]
-        except :
-            Appliances=''       
-        try:  
-            Room=''  
-            for rm  in Listing.find('RichDetails').find('Rooms').findall('Room'): 
-                Room = Room+','+ rm.text 
-            Room=Room[1:]            
-        except :
-            Room=''        
-        try:    
-            Description = Listing.find('BasicDetails').find('Description').text[:200] 
-        except :
-            Description=''        
-        Listings.loc[i] = [misid_,MlsName_,DateListed,StreetAddress,Price,Bedrooms,Bathrooms,Appliances,Room,Description]
+def DataManip(DataF):
+    DataF['DateListed'] =pd.to_datetime(DataF['DateListed'])
+    DataF=DataF.sort_values(by='DateListed')
+    return DataF
 
-Listings['DateListed'] =pd.to_datetime(Listings['DateListed'])
-Listings=Listings.sort_values(by='DateListed')
-Listings.to_csv(r'C:\Users\mandar\Desktop\KaggleData\CodeTestBooj\listings2016.csv',index=False)        
+def csvDownLoad(DataF, Donwnload_Path):    
+    DataF.to_csv(Donwnload_Path,index=False)
+
+def main():
+    ListingDF=loadData('http://syndication.enterprise.websiteidx.com/feeds/BoojCodeTest.xml')
+    FinalDF=DataManip(ListingDF)
+    csvDownLoad(FinalDF, 'C:\Users\mandar\Desktop\KaggleData\CodeTestBooj\listing.csv')
+    
+if __name__ == "__main__":
+     
+    # calling main function
+    main()    
